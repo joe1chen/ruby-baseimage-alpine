7e4fa575e22281a5a838978af3d679c4f8bca1f5
Joseph Chen <joseph.chen@gmail.com>
2019-05-10 18:31:20 -0700

    Remove alpine 3.7 support since there is no ruby 2.6.3 image for alpine 3.7.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for ver in 7 8 9 ; do docker build --pull --no-cache --force-rm=true -t baseimage-alpine:v3.$ver -f Dockerfile.v3.$ver . ; done
+for ver in 8 9 ; do docker build --pull --no-cache --force-rm=true -t ruby-baseimage-alpine:2.6.3-alpine3.$ver -f Dockerfile.v3.$ver . ; done
